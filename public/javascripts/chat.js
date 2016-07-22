d1bf0812879520563e1e2bac1b4c9f9c0a38c59c
Lucas Barbula <lucas.barbula@gmail.com>
2016-07-22 11:01:36 -0600

    setting up client relationship

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
+  let socket = io()
   // TODO: initialize sockets
-  // addSubmitListener(socket);
-  // addResponseListener(socket);
+  addSubmitListener(socket);
+  addResponseListener(socket);
 });
+
+let addSubmitListener = (socket) => {
+  $('form').submit(function(event){
+    event.preventDefault()
+    let message = $('#message').val()
+    socket.emit('chat message', message)
+    $('#message').val('')
+  })
+}
+
+let addResponseListener = (socket) => {
+  socket.on('chat message', function(message) {
+     $('#showMessage').append('<li>' + message + '</li>')
+  })
+}
