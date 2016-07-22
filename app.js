a883f53052e4b6a0a645a7d864f3816618ab120c
Will Thomas <wlthomas001@gmail.com>
2016-07-22 11:15:30 -0600

    working chat app

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var app = express();
-// TODO: create http server
-// TODO: set up socket server
+var server = require('http').Server(app);
+var io = require('socket.io')(server)
+
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, reqs){
+app.get('/', function(req, res){
   res.render('index', {title: 'Socket Review'});
 });
 
-// TODO: bind http server
-// TODO: io stuff
+var port = process.env.PORT || 3000
+
+server.listen(port, function() {
+  console.log("Listening on localhost:3000");
+})
+
+io.on('connection', function (socket) {
+  socket.on('chat message', function (message) {
+    io.emit('chat message', message);
+  })
+})
